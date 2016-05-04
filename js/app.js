4a23cae9935ebaa7fcab6740230c54985c2c5619
nikthequik <quik.the.nick@gmail.com>
2016-05-04 15:10:01 -0600

    -fix html

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ $(function() {
 
 	for (var i=1; i <=100; i++) {
 		if (i % 3 === 0 && i % 5 === 0) {
-			$('body').append("fizz buzz<br>");
+			$('body').append("<p>fizz buzz</p>");
 		}
 		else if (i % 3 === 0) {
-			$('body').append("fizz<br>");
+			$('body').append("<p>fizz</p>");
 		}
 		else if (i % 5 === 0) {
-			$('body').append("buzz<br>");
+			$('body').append("<p>buzz</p>");
 		}
 		else {
-			$('body').append(i + '<br>');
+			$('body').append("<p>" + i + "</p>");
 		}
 	}
 });
