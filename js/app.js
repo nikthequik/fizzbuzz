65caffeeb9a0919a1e1855c651376ba17424acf2
nikthequik <quik.the.nick@gmail.com>
2016-05-04 14:46:01 -0600

    -completion of fizzbuzz 1-100

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	for (var i=1; i <=100; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			$('body').append("fizz buzz<br>");
+		}
+		else if (i % 3 === 0) {
+			$('body').append("fizz<br>");
+		}
+		else if (i % 5 === 0) {
+			$('body').append("buzz<br>");
+		}
+		else {
+			$('body').append(i + '<br>');
+		}
+	}
+});
