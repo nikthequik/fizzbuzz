541076180b387060d0ad9a3bffad35a983390346
nikthequik <quik.the.nick@gmail.com>
2016-05-05 13:12:52 -0600

    -add fizzbuzz function -add onclick event for submit button -complete fizzbuzz refactor

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 $(function() {
 
-
-	for (var i=1; i <=100; i++) {
-		if (i % 3 === 0 && i % 5 === 0) {
-			$('body').append("<b>fizz buzz</b><br>");
-		}
-		else if (i % 3 === 0) {
-			$('body').append("<p>fizz</p>");
-		}
-		else if (i % 5 === 0) {
-			$('body').append("<p>buzz</p>");
+	function fizzbuzz(integer) {
+		integer = parseInt(integer);
+		if (!integer) {
+			alert('That was not a number.  Try again.');
 		}
 		else {
-			$('body').append("<p>" + i + "</p>");
+			for (var i=1; i <= integer; i++) {
+				if (i % 3 === 0 && i % 5 === 0) {
+					$('body').append("<p><b>fizz buzz</b></p><br>");
+				}
+				else if (i % 3 === 0) {
+					$('body').append("<p>fizz</p>");
+				}
+				else if (i % 5 === 0) {
+					$('body').append("<p>buzz</p>");
+				}
+				else {
+					$('body').append("<p>" + i + "</p>");
+				}
+			}
 		}
-	}
+	};
+
+	$('#submission').on('click', function(){
+		var number = $('#userInput').val();
+		fizzbuzz(number); 
+	});
 });
