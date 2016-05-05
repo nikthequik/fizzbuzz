7a427653c8a47ec02d6630226bd40032d5a99643
nikthequik <quik.the.nick@gmail.com>
2016-05-05 12:08:16 -0600

    -add text input -add instructions -add bold to fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(function() {
 
+
 	for (var i=1; i <=100; i++) {
 		if (i % 3 === 0 && i % 5 === 0) {
-			$('body').append("fizz buzz<br>");
+			$('body').append("<b>fizz buzz</b><br>");
 		}
 		else if (i % 3 === 0) {
 			$('body').append("fizz<br>");
